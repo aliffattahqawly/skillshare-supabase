18f90912f5db1e14fe2057708770105ecdc87617
Alif <aliffattah04@gmail.com>
2024-11-16 18:08:33 +0700

    imageSliderAdapter

---
+++ app/src/main/java/com/yourssohail/learnsupabase/ImageSliderAdapter.kt
@@ -0,0 +1,35 @@
+package com.yourssohail.learnsupabase
+
+import com.bumptech.glide.Glide
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ImageSliderAdapter(private val images: List<String>) : RecyclerView.Adapter<ImageSliderAdapter.SliderViewHolder>() {
+
+    // ViewHolder untuk menyimpan referensi ke tampilan item
+    class SliderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    // Membuat ViewHolder baru
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder {
+        // Menginflate layout untuk item slider
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.slider_item, parent, false)
+        return SliderViewHolder(view)
+    }
+
+    // Mengikat data ke ViewHolder
+    override fun onBindViewHolder(holder: SliderViewHolder, position: Int) {
+        // Menggunakan Glide untuk memuat gambar dari URL
+        Glide.with(holder.itemView)
+            .load(images[position]) // Mengambil URL gambar dari daftar
+            .into(holder.imageView) // Memasukkan gambar ke ImageView
+    }
+
+    // Mengembalikan jumlah item dalam daftar
+    override fun getItemCount(): Int = images.size
+}
